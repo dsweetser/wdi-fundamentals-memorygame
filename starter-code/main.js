f584ed91a2aeff13cb9db8b71363276c66d96220
Daniel Sweetser <danielsweetser@Daniels-MacBook-Pro.local>
2017-01-02 23:17:08 -0500

    switched div's over to main.js from index.html

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,9 +4,19 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardFour){
+var board = document.getElementById('game-board');
+var createCards = function(){
+	for (var i = 0; i < 4; i++){
+		var card = document.createElement('div');
+		card.className='card';
+		board.appendChild(card);
+	}
+}
+createCards();
+/*if (cardOne === cardFour){
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again.")
 }
+*/
